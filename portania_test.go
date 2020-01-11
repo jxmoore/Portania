45754925451846f03e4e0e007cdd145d85aaaf7b
joe.moore1988@outlook.com <joe.moore1988@outlook.com>
2020-01-11 02:46:07 -0500

    updating tests

--- portania_test.go
+++ portania_test.go
@@ -5,8 +5,6 @@ import (
 	"time"
 )
 
-//listenOnPort := 8989
-
 // TestPortaniaGetPorts tests the getPorts function in Portania.
 func TestPortaniaGetPorts(t *testing.T) {
 
@@ -58,26 +56,60 @@ func TestPortaniaGetPorts(t *testing.T) {
 	}
 }
 
-// TestPortaniaGetPorts tests the getPorts function in Portania.
+// ExampleConnectionBrokerFail tests the output printed when a connection fails during
+// the connectionBrokers worker go routine
+func ExampleConnectionBrokerFail() {
+
+	connectionBroker(time.Second*5, 3, "localhost", []int{999}, false)
+	// Output: failed to connect to localhost:999 : dial tcp [::1]:999: connect: connection refused
+
+}
+
+// ExampleConnectionBrokerPass tests the output printed when a connection is successful during
+// the connectionBrokers worker go routine
+func ExampleConnectionBrokerPass() {
+
+	connectionBroker(time.Second*5, 3, "google.com", []int{443}, false)
+	// Output: Connected to google.com:443
+}
+
+// TestPortaniaConnection tests the testConnection function in Portania.
 func TestPortaniaConnection(t *testing.T) {
 
 	testSuite := map[string]struct {
 		addr     string
 		duration time.Duration
 		err      string
+		success  bool
 	}{
 		"testConnection should throw an due to a closed port on localhost": {
-			err: "failed ...",
+			addr:     "localhost:555",
+			success:  false,
+			duration: time.Second * 5,
+			err:      "dial tcp [::1]:555: connect: connection refused",
+		},
+		"testConnection should return true indicating that the port is open": {
+			addr:     "github.com:443",
+			duration: time.Second * 5,
+			success:  true,
 		},
 	}
+
 	for testName, testCase := range testSuite {
-		t.Logf("Running test %v\n%v", testName, testCase)
+
+		t.Logf("Running test %v\n", testName)
+
+		ok, err := testConnection(testCase.addr, testCase.duration)
+		if err != nil && err.Error() != testCase.err {
+			t.Errorf("expected testConnection to fail with %v but received %v\n", testCase.err, err.Error())
+		} else {
+			t.Logf("received the expected error result %v\n", testCase.err)
+		}
+
+		if ok != testCase.success {
+			t.Errorf("expected testConnection to return %v but received %v\n", testCase.success, ok)
+		} else {
+			t.Logf("received the expected response from testConnection\n")
+		}
 	}
-}
-
-func ExampleconnectionBroker() {
-
-	// Output:
-	// Connected to localhost:8989
-
 }
